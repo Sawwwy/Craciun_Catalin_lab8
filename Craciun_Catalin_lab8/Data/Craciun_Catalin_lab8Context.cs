15b4d116f23c63b66dc385fd943dfab0539f67fd
Craciun Catalin <savanacatalin@yahoo.com>
2021-12-07 19:01:49 +0200

    Add project files.

---
+++ Craciun_Catalin_lab8/Data/Craciun_Catalin_lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Craciun_Catalin_lab8.Models;
+
+namespace Craciun_Catalin_lab8.Data
+{
+    public class Craciun_Catalin_lab8Context : DbContext
+    {
+        public Craciun_Catalin_lab8Context (DbContextOptions<Craciun_Catalin_lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Craciun_Catalin_lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Craciun_Catalin_lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Craciun_Catalin_lab8/Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Craciun_Catalin_lab8.Data;
+using Craciun_Catalin_lab8.Models;
+
+namespace Craciun_Catalin_lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Craciun_Catalin_lab8.Data.Craciun_Catalin_lab8Context _context;
+
+        public IndexModel(Craciun_Catalin_lab8.Data.Craciun_Catalin_lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
